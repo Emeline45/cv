d65dd3fb24da55c269b012690d5e8c3e0503a978
Emeline45 <54811102+Emeline45@users.noreply.github.com>
2022-04-07 09:32:21 +0200

    add front contact

---
+++ accueil/contact.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <div class="text-center">
+                        <h2>Contactez-moi</h2>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="text-center">
+                            <h3>Pour plus de renseignements contactez-moi.</h3>
+                            <br>
+                            <p>Vous pouvez me contacter via mon site internet ou mes réseaux sociaux.</p>
+                                <button type="button" class="btn btn-outline-primary" onclick="window.location.href = 'https://www.facebook.com/profile.php?id=100003404464118';"><i class="fab fa-facebook-f"></i></button>
+                                <button type="button" class="btn btn-outline-info" onclick="window.location.href = 'https://www.linkedin.com/in/emeline-bonte-1651881b7';"><i class="fab fa-linkedin-in"></i></button>
+                                <button type="button" class="btn btn-outline-dark" onclick="window.location.href = 'https://github.com/Emeline45';"><i class="fab fa-github"></i></button>
+                            <p>Vous pouvez également me joindre au : <a href="tel:+33614360795">06 14 36 07 95</a></p>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <form class="row g-3"  style="margin-top: 0 !important;">
+                                <div class="col-md-6">
+                                    <label for="prenom" class="form-label">Prénom*</label>
+                                    <input type="text" class="form-control" id="prenom">
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="nom" class="form-label">Nom*</label>
+                                    <input type="text" class="form-control" id="nom">
+                                </div>
+                                <div class="col-md-12">
+                                    <label for="inputEmail4" class="form-label">Email*</label>
+                                    <input type="email" class="form-control" id="inputEmail4">
+                                </div>
+                                <div class="col-12">
+                                    <label for="inputEmail4" class="form-label">Commentaire*</label>
+                                    <div class="form-floating">
+                                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary">Envoyer</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+        <br>
+    </body>
+</html>
